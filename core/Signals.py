9b6e0dd3079019156102a17ef35ef4fb5c460816
Mateusz Hołenko <mholenko@antmicro.com>
2014-06-05 09:48:38 +0200

    Urwid signal registration bug fixed.

--- core/Signals.py
+++ core/Signals.py
@@ -3,12 +3,15 @@ import urwid.signals
 
 _registered_signals = {}
 
+
 def register_signal(obj, name):
     if obj.__class__ not in _registered_signals:
-        urwid.register_signal(obj.__class__, name)
-        _registered_signals[obj.__class__] = True
+        _registered_signals[obj.__class__] = []
+    if name not in _registered_signals[obj.__class__]:
+        _registered_signals[obj.__class__].append(name)
+        urwid.register_signal(obj.__class__, _registered_signals[obj.__class__])
+
 
 def emit_signal(obj, name, args):
     register_signal(obj, name)
     urwid.emit_signal(obj, name, args)
-
